24516656829685496b8594e3e51b5fa297132341
KANG-JUNHA <97940615+KANG-JUNHA@users.noreply.github.com>
2023-01-20 16:48:36 +0900

    EraseColor
    erase and destory canvas

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const lineWidth = document.getElementById("line-width");
 const color = document.getElementById("color");
 const colorOptions = Array.from(document.getElementsByClassName("color-option")) // Array.from으로 배열 생성해야 forEach를 사용할 수 있음
 const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 
-canvas.width=800;
-canvas.height=800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width=CANVAS_WIDTH;
+canvas.height=CANVAS_HEIGHT;
 ctx.lineWidth=lineWidth.value; //canvas width/height 아래에 lineWidth코드를 넣어야 적용 됨.
 
 let isPainting = false;
@@ -59,8 +64,19 @@ function onModeClick(){
 
 function onCanvasClick(){
     if(isFilling){
-        ctx.fillRect(0,0,800,800);
+        ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
     }
+}
+
+function onDestroyClick(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+}
+
+function onEraserClick(){
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
 }
 
 canvas.addEventListener("mousemove",onMove);
@@ -75,4 +91,8 @@ colorOptions.forEach((color)=>color.addEventListener("click",onColorClick)); // 
 
 modeBtn.addEventListener("click",onModeClick); // draw, fill 선택하기
 canvas.addEventListener("click",onCanvasClick); //canvas fill하기
+
+destroyBtn.addEventListener("click",onDestroyClick); // canvas 전체 다 지우기
+eraserBtn.addEventListener("click",onEraserClick); // canvas 부분 지우기
+
 
